a03eb6f0996e916aaaf99a0e54ea596b21dc73d3
Alex Calabrese <58480609+alexcalabrese@users.noreply.github.com>
2022-08-31 12:53:49 +0200

    Time: 0 ms (100.00%), Space: 51.2 MB (70.33%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -13,11 +13,25 @@
  *     }
  * }
  */
+
+// Iterative
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {  
+        while(root != null){
+            if(root.val == val) return root;
+            
+            root = val > root.val ? root.right : root.left;
+        }
+        
+        return root;
+    }
+}
+
+/* 
+Recursive
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return root;
-        
-        if(root.val == val) return root;
+        if(root == null || root.val == val) return root;
         
         if(val > root.val)
             return searchBST(root.right, val);
@@ -25,3 +39,4 @@ class Solution {
             return searchBST(root.left, val);
     }
 }
+*/
