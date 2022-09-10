e60684b111c26168b6d0700f77e04e9661fc6233
Alex Calabrese <58480609+alexcalabrese@users.noreply.github.com>
2022-09-10 18:00:47 +0200

    Time: 1 ms (80.29%), Space: 43.3 MB (28.81%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -8,32 +8,64 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {        
+class Solution {    
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode();
-        ListNode curr = new ListNode();
-        dummy.next = curr;
+        mergeTwoLists(list1, list2, dummy);
         
-        while(list1 != null || list2 != null){
-            if(list1 != null && list2 != null){
-                if(list1.val < list2.val){
-                    curr.next = list1;
-                    list1 = list1.next;
-                } else {
-                    curr.next = list2;
-                    list2 = list2.next;
-                }
-            } else {            
-                if(list1 == null) curr.next = list2;
-                else if(list2 == null) curr.next = list1;
-                
-                if(list1 != null) list1 = list1.next;
-                if(list2 != null) list2 = list2.next;
-            }
-            
-            curr = curr.next;
+        return dummy.next;
+    }
+    
+    public void mergeTwoLists(ListNode list1, ListNode list2, ListNode curr) {
+        if(list1 == null && list2 == null) return;
+        
+        if(list1 == null && list2 != null){
+            curr.next = list2;
+            return;
         }
         
-        return dummy.next.next;
+        if(list2 == null && list1 != null){
+            curr.next = list1;
+            return;
+        }
+        
+        if(list1.val < list2.val){
+            curr.next = list1;
+            list1 = list1.next;
+        } else {
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        
+        mergeTwoLists(list1, list2, curr.next);
     }
+    
+// Iterative
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {        
+//         ListNode dummy = new ListNode();
+//         ListNode curr = new ListNode();
+//         dummy.next = curr;
+        
+//         while(list1 != null || list2 != null){
+//             if(list1 != null && list2 != null){
+//                 if(list1.val < list2.val){
+//                     curr.next = list1;
+//                     list1 = list1.next;
+//                 } else {
+//                     curr.next = list2;
+//                     list2 = list2.next;
+//                 }
+//             } else {            
+//                 if(list1 == null) curr.next = list2;
+//                 else if(list2 == null) curr.next = list1;
+                
+//                 if(list1 != null) list1 = list1.next;
+//                 if(list2 != null) list2 = list2.next;
+//             }
+            
+//             curr = curr.next;
+//         }
+        
+//         return dummy.next.next;
+//     }
 }
